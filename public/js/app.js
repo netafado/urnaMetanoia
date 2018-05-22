41b3ebcdc94e25923a82e49914f7b53e7311ccb9
Isaias Francisco dos Santos <isaias.fran@gmail.com>
2018-05-21 23:37:21 -0300

    urna

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,32 @@
 
 (function (){
-	
+    // sons
+    var finalizarSom    = document.getElementById('sonFinalizar');
+    var clique          = document.getElementById('somClique'); 
+    //botoes
+    var btns            = document.querySelectorAll('.clicavel');
+    var confirmar       = document.getElementById('confirmar');
+    var corrigir        = document.getElementById('corrigir')
+    // manifesto
+    var manifesto       = document.getElementById('manifesto');
+
+    btns.forEach(function(element){
+        element.addEventListener('click', function(){
+            clique.play();
+            console.log(this.dataset.number);
+            if( manifesto.innerHTML === "00")
+                manifesto.innerHTML = ""
+            manifesto.innerHTML += this.dataset.number
+
+            if(manifesto.innerHTML.length > 2)
+                manifesto.innerHTML = "00"
+        })
+    })
+
+    confirmar.addEventListener('click', function(){
+        finalizarSom.play();
+    })
+
+
+    
 })()
